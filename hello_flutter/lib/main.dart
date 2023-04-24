e942c15dd3f4343954b7df91203fb17648142166
KeonhoPark <gse04063@gmail.com>
2023-04-24 22:03:05 +0900

    Flutter 맛보기 - 로그인 페이지 만들기

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -12,7 +12,49 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text(
+            'Hello Flutter',
+            style: TextStyle(fontSize: 28),
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.network(
+                    "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                    width: 81,
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'email'),
+                  style: TextStyle(fontSize: 24),
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'password'),
+                  style: TextStyle(fontSize: 24),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 16),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
