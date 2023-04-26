218b972858a10f831b9629e4500a201f82ed82ee
KeonhoPark <gse04063@gmail.com>
2023-04-26 21:29:38 +0900

    인스타그램 클론코딩(좋아요, 피드리스트 만들기)

--- instagram_clone/lib/home_page.dart
+++ instagram_clone/lib/home_page.dart
@@ -9,30 +9,34 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              CupertinoIcons.camera,
-              color: Colors.black,
-            )),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              CupertinoIcons.paperplane,
-              color: Colors.black,
-            ),
-          )
-        ],
-        title: Image.asset(
-          'assets/logo.png',
-          height: 32,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.camera,
+                color: Colors.black,
+              )),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.paperplane,
+                color: Colors.black,
+              ),
+            )
+          ],
+          title: Image.asset(
+            'assets/logo.png',
+            height: 32,
+          ),
         ),
-      ),
-      body: Feed(),
-    );
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Feed();
+          },
+        ));
   }
 }
