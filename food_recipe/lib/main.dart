998b17dd0207f2f12498a84db2f1bcbc8a76cc9e
KeonhoPark <gse04063@gmail.com>
2023-04-26 18:32:11 +0900

    실습1(drawerHeader)

--- food_recipe/lib/main.dart
+++ food_recipe/lib/main.dart
@@ -48,87 +48,122 @@ class HomePage extends StatelessWidget {
     ];
 
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          centerTitle: false,
-          iconTheme: IconThemeData(color: Colors.black),
-          title: Text("Food Recipe",
-              style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black)),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  print("go to mypage");
-                },
-                icon: Icon(
-                  Icons.person_outline,
-                ))
-          ],
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "상품을 입력해주세요.",
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      print("search");
-                    },
-                    icon: Icon(Icons.search),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            centerTitle: false,
+            iconTheme: IconThemeData(color: Colors.black),
+            title: Text("Food Recipe",
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black)),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    print("go to mypage");
+                  },
+                  icon: Icon(
+                    Icons.person_outline,
+                  ))
+            ],
+          ),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "상품을 입력해주세요.",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        print("search");
+                      },
+                      icon: Icon(Icons.search),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: foodData.length,
-                    itemBuilder: (context, index) {
-                      Map<String, dynamic> data = foodData[index];
-                      String category = data["category"];
-                      dynamic imgUrl = data["imgUrl"];
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: foodData.length,
+                      itemBuilder: (context, index) {
+                        Map<String, dynamic> data = foodData[index];
+                        String category = data["category"];
+                        dynamic imgUrl = data["imgUrl"];
 
-                      return Card(
-                        margin: const EdgeInsets.all(8),
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Image.network(
-                              imgUrl,
-                              width: double.infinity,
-                              height: 120,
-                              fit: BoxFit.cover,
-                            ),
-                            Container(
-                              alignment: Alignment.center,
-                              width: double.infinity,
-                              height: 120,
-                              color: Colors.black.withOpacity(0.5),
-                              child: Text(
-                                category,
-                                style: TextStyle(
-                                  fontSize: 36,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                        return Card(
+                          margin: const EdgeInsets.all(8),
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Image.network(
+                                imgUrl,
+                                width: double.infinity,
+                                height: 120,
+                                fit: BoxFit.cover,
                               ),
-                            )
-                          ],
-                        ),
-                      );
-                    }))
-          ],
-        ),
-      ),
-    );
+                              Container(
+                                alignment: Alignment.center,
+                                width: double.infinity,
+                                height: 120,
+                                color: Colors.black.withOpacity(0.5),
+                                child: Text(
+                                  category,
+                                  style: TextStyle(
+                                    fontSize: 36,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      }))
+            ],
+          ),
+          drawer: Drawer(
+            child: Column(
+              children: [
+                DrawerHeader(
+                    decoration: BoxDecoration(color: Colors.amber),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: CircleAvatar(
+                              radius: 36,
+                              backgroundColor: Colors.white,
+                              child: Image.network(
+                                "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                                width: 40,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "닉네임",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "hello@world.com",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        ));
   }
 }
