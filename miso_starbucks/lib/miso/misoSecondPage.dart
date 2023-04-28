0ab9514fc8322173dfeb0b3054886acaaf79bdf6
KeonhoPark <gse04063@gmail.com>
2023-04-28 20:31:21 +0900

    실습2 Miso 페이지2

--- miso_starbucks/lib/miso/misoSecondPage.dart
+++ miso_starbucks/lib/miso/misoSecondPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:miso_starbucks/miso/miso.dart';
 
 class MisoSecondPage extends StatelessWidget {
   const MisoSecondPage({Key? key}) : super(key: key);
@@ -7,8 +8,74 @@ class MisoSecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Miso 1째 페이지"),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Stack(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.15,
+                    ),
+                    Text(
+                      "예약내역",
+                      style:
+                          TextStyle(fontSize: 32, fontWeight: FontWeight.w900),
+                    ),
+                    SizedBox(
+                      height: 64,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.error,
+                          color: misoPrimaryColor,
+                        ),
+                        Expanded(
+                            child: FittedBox(
+                          // fit: BoxFit.fitHeight,
+                          child: Text(
+                            "예약된 서비스가 아직 없어요. 지금 예약해보세요!",
+                            style: TextStyle(fontSize: 100),
+                          ),
+                        )),
+                      ],
+                    ),
+                    Divider(
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 40,
+                left: 24,
+                right: 24,
+                child: GestureDetector(
+                    onTap: () => print("예약하기 클릭 됨"),
+                    child: Container(
+                      color: misoPrimaryColor,
+                      width: double.infinity,
+                      height: 58,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "예약하기",
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
