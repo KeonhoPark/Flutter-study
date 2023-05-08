ca0865b51c0460f490920af4aea998262ffa4969
KeonhoPark <gse04063@gmail.com>
2023-05-08 23:03:36 +0900

    버킷 리스트 앱에 Provider 적용하기

--- bucket_list_with_provider/lib/bucket_service.dart
+++ bucket_list_with_provider/lib/bucket_service.dart
@@ -5,4 +5,19 @@ class BucketService extends ChangeNotifier {
   List<Bucket> bucketList = [
     Bucket('잠자기', false),
   ];
+
+  void createBucket(String job) {
+    bucketList.add(Bucket(job, false));
+    notifyListeners();
+  }
+
+  void updateBucket(Bucket bucket, int index) {
+    bucketList[index] = bucket;
+    notifyListeners();
+  }
+
+  void deleteBucket(int index) {
+    bucketList.removeAt(index);
+    notifyListeners();
+  }
 }
